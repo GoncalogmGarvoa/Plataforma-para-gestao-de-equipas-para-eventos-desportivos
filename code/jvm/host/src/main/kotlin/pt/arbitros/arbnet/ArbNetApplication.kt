4751c3de82e904e5978d9b2ab62c4db789a82b37
samot2769 <A47260@alunos.isel.pt>
2025-04-17 19:08:55 +0100

    fixed tables

--- code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
+++ code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
@@ -2,6 +2,7 @@ package pt.arbitros.arbnet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
