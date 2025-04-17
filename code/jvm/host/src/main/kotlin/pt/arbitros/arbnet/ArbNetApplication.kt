772227733b756f635a084679fb7a622f4095718d
GB <72549327+GoncalogmGarvoa@users.noreply.github.com>
2025-04-17 19:20:07 +0100

    Revert "fixed tables"
    This reverts commit 4751c3de82e904e5978d9b2ab62c4db789a82b37.

--- code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
+++ code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
@@ -2,7 +2,6 @@ package pt.arbitros.arbnet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
---
+++ src/test/kotlin/pt/arbitros/arbnet/ArbNetApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.arbitros.arbnet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArbNetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
