fea639dd827a6555817e3da4438659c2542991c1
samot2769 <A47260@alunos.isel.pt>
2025-04-17 19:17:57 +0100

    fixed tables

--- code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
+++ code/jvm/host/src/main/kotlin/pt/arbitros/arbnet/ArbNetApplication.kt
@@ -3,20 +3,22 @@ package pt.arbitros.arbnet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+import org.springframework.transaction.TransactionManager
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
 class ArbNetApplication {
-    @Bean
-    fun jdbi() =
-        Jdbi
-            .create(
-                PGSimpleDataSource().apply {
-                    setURL(Environment.getDbUrl())
-                },
-            ).configureWithAppRequirements()
-
+//    @Bean
+//    fun jdbi() =
+//        Jdbi
+//            .create(
+//                PGSimpleDataSource().apply {
+//                    setURL(Environment.getDbUrl())
+//                },
+//            ).configureWithAppRequirements()
+//
 //    @Bean
 //    @Profile("jdbi")
 //    fun trxManagerJdbi(jdbi: Jdbi): TransactionManager = TransactionManagerJdbi(jdbi)
--- code/jvm/host/src/test/kotlin/pt/arbitros/arbnet/ArbNetApplicationTests.kt
+++ code/jvm/host/src/test/kotlin/pt/arbitros/arbnet/ArbNetApplicationTests.kt
@@ -2,6 +2,7 @@ package pt.arbitros.arbnet
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.testng.annotations.Test
 
 @SpringBootTest
 class ArbNetApplicationTests {
