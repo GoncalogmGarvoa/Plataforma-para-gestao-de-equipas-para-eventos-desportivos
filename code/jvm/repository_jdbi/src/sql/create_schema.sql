1e67d3a748c4da993fb0f0e1ad570a24e7b859f0
samot2769 <A47260@alunos.isel.pt>
2025-04-17 18:45:47 +0100

    fixed tables

--- code/jvm/repository_jdbi/src/sql/create_schema.sql
+++ code/jvm/repository_jdbi/src/sql/create_schema.sql
@@ -28,7 +28,7 @@ create table dbp.referee (
 
 create table dbp.arbitration_council (
                                      user_id int primary key,
-                                     foreign key (user_id) dbp.references user(id)
+                                     foreign key (user_id) references dbp.user(id)
 );
 
 create table dbp.category_dir (
@@ -38,8 +38,8 @@ create table dbp.category_dir (
                               end_date date,
                               category_id int,
                               primary key (id, referee_id, category_id),
-                              foreign key (referee_id) dbp.references referee(user_id),
-                              foreign key (category_id) dbp.references category(id)
+                              foreign key (referee_id) references dbp.referee(user_id),
+                              foreign key (category_id) references dbp.category(id)
 );
 
 create table dbp.competition (
@@ -73,7 +73,7 @@ create table dbp.match_day (
                        match_date date,
                        competition_id int,
                        primary key (id, competition_id),
-                       foreign key (competition_id) dbp.references competition(id)
+                       foreign key (competition_id) references dbp.competition(id)
 );
 
 create table dbp.participant (
@@ -84,10 +84,10 @@ create table dbp.participant (
                          role_id int,
                          confirmation_status varchar(20) check (confirmation_status in ('waiting', 'accepted', 'declined')),
                          primary key (call_list_id, match_day_id, referee_id, role_id),
-                         foreign key (role_id) dbp.references role(id),
-                         foreign key (call_list_id) dbp.references call_list(id),
-                         foreign key (match_day_id, competition_id_match_day) dbp.references match_day(id, competition_id),
-                         foreign key (referee_id) dbp.references referee(user_id)
+                         foreign key (role_id) references dbp.role(id),
+                         foreign key (call_list_id) references dbp.call_list(id),
+                         foreign key (match_day_id, competition_id_match_day) references dbp.match_day(id, competition_id),
+                         foreign key (referee_id) references dbp.referee(user_id)
 );
 
 create table dbp.session (
@@ -97,7 +97,7 @@ create table dbp.session (
                          match_day_id int,
                          competition_id_match_day int,
                          primary key (id, match_day_id, competition_id_match_day),
-                         foreign key (match_day_id, competition_id_match_day) dbp.references match_day(id, competition_id)
+                         foreign key (match_day_id, competition_id_match_day) references dbp.match_day(id, competition_id)
 );
 
 create table dbp.position (
@@ -111,9 +111,9 @@ create table dbp.session_referees (
                                   referee_id int,
                                   match_day_id_session int,
                                   primary key (position_id, session_id, referee_id, match_day_id_session),
-                                  foreign key (session_id, match_day_id_session) dbp.references session(id, match_day_id),
-                                  foreign key (referee_id) dbp.references referee(user_id),
-                                  foreign key (position_id) dbp.references position(id)
+                                  foreign key (session_id, match_day_id_session) references dbp.session(id, match_day_id),
+                                  foreign key (referee_id) references dbp.referee(user_id),
+                                  foreign key (position_id) references dbp.position(id)
 );
 
 create table dbp.report (
